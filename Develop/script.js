cbb77ff03810b33fe5972c882a3e0675daa6d796
Alux <plutocoding@gmail.com>
2023-08-14 23:31:40 -0400

    fixing javascript, adding 2 readme

--- Develop/script.js
+++ Develop/script.js
@@ -8,15 +8,15 @@ const numericalValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialValues = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+'];
 //The below array is so that when the user chooses to have different types of characters in the password,
 //those values in the other selected arrays will be added to this new custom array.
-var customUserArray = [];
 
 // The Function creates the password and returns it so that it can be used by the writePassword function
 function generatePassword() {
-  var length = prompt("How long do you want your password to be? (8 characters minimum and 120 maximum.")
+  var customUserArray = [];
+  var length = prompt("How long do you want your password to be? (8 characters minimum and 120 maximum.");
   //For testing purposes, we are logging what the user writes.
   console.log("Selected Password Length:", length); 
   //We are checking to see if the input is a numerical value
-  if (length.isInteger() === false) {
+  if (isNaN(length) === true) {
     alert("We can't choose a password length unless you only type in numbers! Please Try again.")
     return
   }
@@ -43,17 +43,18 @@ function generatePassword() {
   }
 
 //Adding the selected arrays into the custom array
+//Pushing entire tables into the custom table
   if (lowerConfirm) {
-    customUserArray.push(alphabetLower);
+    customUserArray = customUserArray.concat(alphabetLower);
   }
   if (upperConfirm) {
-    customUserArray.push(alphabetUpper);
+    customUserArray = customUserArray.concat(alphabetUpper);
   }
   if (numberConfirm) {
-    customUserArray.push(numericalValues);
+    customUserArray = customUserArray.concat(numericalValues);
   }
   if (specialConfirm) {
-    customUserArray.push(specialValues);
+    customUserArray = customUserArray.concat(specialValues);
   }
 
   //An empty password variable which will store the generated password from the generatePassword function
@@ -62,10 +63,10 @@ function generatePassword() {
   //Creating the password generation for loop
   // For loops which loops through an amount of times based on the selected length of the variable "length"
   //Lopps through the custom array to grab characters each time
-  for (i=0; i<=length.length; i++) {
-    newPassword += Math.floor(Math.random() * length.length)
+  for (i=0; i<length; i++) {
+    newPassword += customUserArray[Math.floor(Math.random() * customUserArray.length)]
   }
-  
+  console.log(customUserArray);
   //After the for loop ends, we return the adjusted variable as the value that the function generates.
   return newPassword
 }
@@ -82,3 +83,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// My Questions for the tutor
+// - I was getting errors on line 19; saying something about length.isInteger() not being a valid function
+
+// script.js:19 Uncaught TypeError: length.isInteger is not a function
+//at generatePassword (script.js:19:14)
+//at HTMLButtonElement.writePassword (script.js:76:18)
+
+
+
+
+
+// - Is there a way to force the user into only typing a number?
+// - Is there a way to make the user get re-promopted from where they left off instead of restarting the entire process again?
+// - An example of above: line 20, 25, 42
+// - How do I alert the user after an action before they continue on with the process?
+// - On line 85, in the addEventListener function, why does the writePassword function not have ().
+// - Is it because functions inside of parameters can't have parameters themselves?
+// - How do I add a video or gif that auto loops on a website?
+// - How do I make the position of my navigation bar move when scrolling but then stick to the top of the screen? (Sticky?)
+// - What would I do to customize the prompts/confirms - positioning, size, styling
+// - Sometime when I create my git hub page for my repo, instead of the index.html, it displays the readme. Any ideas?
+// - Something that seemed to work last time, was I added the index.html file back into the main repo folder, and/or re-added the readme aswell
+// - I had trouble with displaying a background image in the hhtml document, and had to create it in the css file, how do I properly add it to the background?
--- README.md
+++ README.md
@@ -5,6 +5,11 @@ Link to Page: https://plutocoders.github.io/passwordGen3/
 Develop folder contains files.
 - a style.css file, and a script.js file.
 - the script.js file contains the project code, and is commented out explaining what i'm trying to do in each section.
+- image of page:
+<img src=Assets/pageimage.png>
+![Here is the link](Assets/pageimage.png)
+
+
 
 ## Credits
 - I used class resources such as class notes and rewatching zoom sessions to help me complete this project.
@@ -14,3 +19,4 @@ Develop folder contains files.
 - I used chatgpt for quick answers when I came across issues
 - An example of how I used chat gpt: "Can you write out all the letters in the english alphabet, in lower case, with each letter in quotes?" (This helped me make the arrays much faster)
 - I also had help during office hours after class from the TA and Professor.
+- I had a tutor session with Armando to help me fix up my errors and resubmit the assignment for a better great.
